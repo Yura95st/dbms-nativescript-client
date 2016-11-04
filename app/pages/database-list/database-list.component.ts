875d194b367dd012afac845507c04797d0638495
yura95st <yura95st@gmail.com>
2016-11-04 22:27:29 +0200

    Initial commit.

---
+++ app/pages/database-list/database-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "pages/database-list/database-list.html",
+  styleUrls: ["pages/database-list/database-list-common.css", "pages/database-list/database-list.css"],
+})
+export class DatabaseListComponent {
+}
